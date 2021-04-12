37211ce7c0b36669c4eb70e7ea11ca47d02c17e2
LenhardErwan <35600901+LenhardErwan@users.noreply.github.com>
2021-04-12 19:49:57 +0200

    feat: default view

--- __manifest__.py
+++ __manifest__.py
@@ -3,5 +3,6 @@
     'description': 'Report manage intervention reports',
     'author': 'Castel Jeremy, Lenhard Erwan, Very-Griette Milan',
     'depends': ['base'],
+		'data': ['views/tr_global_report.xml'],
     'application': True,
 }
