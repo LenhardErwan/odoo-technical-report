327f910982d06c76517217a69327560de225dcc2
LenhardErwan <35600901+LenhardErwan@users.noreply.github.com>
2021-04-13 14:58:52 +0200

    feat: add block view

--- __manifest__.py
+++ __manifest__.py
@@ -3,6 +3,6 @@
     'description': 'Report manage intervention reports',
     'author': 'Castel Jeremy, Lenhard Erwan, Very-Griette Milan',
     'depends': ['base'],
-		'data': ['views/tr_global_report.xml', 'views/tr_report.xml'],
+		'data': ['views/tr_global_report.xml', 'views/tr_report.xml', 'views/tr_block.xml'],
     'application': True,
 }
