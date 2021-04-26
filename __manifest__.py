14489437323a435bfb01ca13a97a6edd2ebcb4de
LenhardErwan <35600901+LenhardErwan@users.noreply.github.com>
2021-04-26 22:12:11 +0200

    feat: security

--- __manifest__.py
+++ __manifest__.py
@@ -3,6 +3,12 @@
     'description': 'Report manage intervention reports',
     'author': 'Castel Jeremy, Lenhard Erwan, Very-Griette Milan',
     'depends': ['base'],
-		'data': ['views/tr_global_report.xml', 'views/tr_report.xml', 'views/tr_block.xml'],
+    'data': [
+			'security/groups.xml',
+			'security/ir.model.access.csv',
+			'views/tr_global_report.xml',
+			'views/tr_report.xml',
+			'views/tr_block.xml'
+		],
     'application': True,
 }
