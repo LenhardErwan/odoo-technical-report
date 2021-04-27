3a99c15376eee644d3958d5728b02a0015e8c1cc
LenhardErwan <35600901+LenhardErwan@users.noreply.github.com>
2021-04-27 23:18:05 +0200

    feat: Attempt to display the presence of an image

--- models/tr_block.py
+++ models/tr_block.py
@@ -5,7 +5,16 @@ class Block(models.Model):
     _name = 'tr.block'
     _description = 'Block Content'
 
+    image_exist = fields.Boolean(compute='compute_image')
+
     order = fields.Integer('Order')
     image = fields.Image('Image')
     text = fields.Text('Text')
     report_id = fields.Many2one('tr.report', string='Report ID')
+
+    def compute_image(self):
+        for block in self:
+            if block.image:
+                block.image_exist = True
+            else:
+                block.image_exist = True
